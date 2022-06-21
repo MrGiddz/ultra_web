b7bbd5ff7959f87d6069448ca907d0e7454ab51b
MrGiddz <mideolaniyi@outlook.com>
2022-06-21 10:12:21 +0100

    doc folder added

---
+++ docs/precache-manifest.903660a3f672294e7d85ecb6d2781636.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db73223fa7f11bd3588",
+    "url": "/static/js/main.5db73223.chunk.js"
+  },
+  {
+    "revision": "dcdab150751bd3eaafaf",
+    "url": "/static/js/1.dcdab150.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74d550ed75063e796fcaa16fda839a3f",
+    "url": "/static/media/svg-1.74d550ed.svg"
+  },
+  {
+    "revision": "f52007c1f7e9441908ab882400060de2",
+    "url": "/static/media/svg-2.f52007c1.svg"
+  },
+  {
+    "revision": "23437c5f2fc745cb2142c7f12d582c43",
+    "url": "/static/media/profile.23437c5f.jpg"
+  },
+  {
+    "revision": "2e94c7f6aab61172fd8013d92840522e",
+    "url": "/static/media/svg-3.2e94c7f6.svg"
+  },
+  {
+    "revision": "e40035d0d8fc55d673c393773a728b6a",
+    "url": "/index.html"
+  }
+];
